ffd00fc9361e4fa68392fabe912a4ea18039bd1b
zhoushoujian <zhoushoujianwork@163.com>
2024-11-06 20:09:26 +0800

    Fix FlinkV12ClusterList function to use correct cluster name

--- pkg/service/flink_v12_test.go
+++ pkg/service/flink_v12_test.go
@@ -11,7 +11,7 @@ import (
 func TestFlinkV12ClusterList(t *testing.T) {
 	resp, err := k8s.FlinkV12ClusterList("test", model.FilterFlinkV12{
 		NameSpace: tea.String("flink"),
-		// Name:      tea.String("app-session"),
+		Name:      tea.String("bi-matrix-session"),
 		// Owner: tea.String("dingyingjie"),
 	})
 	if err != nil {
