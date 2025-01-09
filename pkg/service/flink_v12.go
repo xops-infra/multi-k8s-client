ef1e1b9fa09a78f6aec1106a7061f1201dbe0845
zhoushoujian <zhoushoujianwork@163.com>
2025-01-09 16:19:06 +0800

    refactor: remove fmt

--- pkg/service/flink_v12.go
+++ pkg/service/flink_v12.go
@@ -107,7 +107,6 @@ func (s *K8SService) FlinkV12ClusterList(k8sClusterName string, filter model.Fil
 			})
 			if err == nil {
 				for k, item := range lbResp.Items {
-					fmt.Println(item.Spec.Type)
 					// 只记录 LoadBalancer 信息
 					if item.Spec.Type == "LoadBalancer" {
 						v.Status.(map[string]any)[fmt.Sprintf("loadbalance-%d", k)] = fmt.Sprintf("%s:%d", item.Status.LoadBalancer.Ingress[0].IP, item.Spec.Ports[0].Port)
